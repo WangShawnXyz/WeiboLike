01234f616afd5116186bad02f039315a2c18f5a3
WangShawnDemo <744585345@qq.com>
2017-03-26 22:52:49 +0800

    finsh 3.0 have todo header

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,8 +2,24 @@
 <html>
 	<head>
 		<title>@yield('title','Myweb App') - Learn Laravel</title>
+		<link rel="stylesheet" type="text/css" href="/css/app.css">
 	</head>
 	<body>
-		@yield('content')
+		<header class="navbar navbar-fixed-top navbar-inverse">
+			<div class="container">
+				<div class="col-md-offset-1 col-md-10">
+					<a href="/" id="logo">Myweb</a>
+					<nav>
+						<ul class="nav navbar-nav navbar-right">
+							<li><a href="/help">Help</a></li>
+							<li><a href="#">login</a></li>
+						</ul>
+					</nav>
+				</div>
+			</div>
+		</header>
+		<div class="container">
+			@yield('content')
+		</div>
 	</body>
 </html>
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,5 +1,17 @@
 @extends('layouts.default')
 @section('title', 'Home')
 @section('content')
-	<h1>Home Page</h1>
+	<div class="jumbotron">
+		<h1>Hello Laravel!</h1>
+		<p class="lead">
+			This is the home page of Learn Laravel!
+		</p>
+		<p>
+			Everything start from here!
+		</p>
+		<p>
+			<!-- register button -->
+			<a class="btn btn-lg btn-success" href="#" role="button">Sign up now!</a>
+		</p>
+	</div>
 @stop
